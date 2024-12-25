2f25065f00591f160fabe5760fbcd63c25a555de
me <me>
2024-12-25 23:05:51 +0300

    Client

--- src/main/java/com/agate/agate/service/ClientService.java
+++ src/main/java/com/agate/agate/service/ClientService.java
@@ -1,9 +1,7 @@
 package com.agate.agate.service;
 
 import com.agate.agate.repository.Entity.Client;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface ClientService {
 
     void setClient(Client client);
--- src/main/java/com/agate/agate/service/impl/ClientServiceImpl.java
+++ src/main/java/com/agate/agate/service/impl/ClientServiceImpl.java
@@ -3,7 +3,9 @@ package com.agate.agate.service.impl;
 import com.agate.agate.repository.ClientRepository;
 import com.agate.agate.repository.Entity.Client;
 import com.agate.agate.service.ClientService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ClientServiceImpl implements ClientService {
 
     ClientRepository clientRepository;
