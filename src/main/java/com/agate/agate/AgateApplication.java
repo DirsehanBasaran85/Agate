7b8576ab6a5e2aae2f21844818a4983bab1d911b
me <me>
2024-12-25 20:50:14 +0300

    init

---
+++ src/main/java/com/agate/agate/AgateApplication.java
@@ -0,0 +1,13 @@
+package com.agate.agate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agate/agate/AgateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agate.agate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
